1f7916b196e3b12e687355099ce6b36ad6acdb93
wangxin <binaryaoe@163.com>
2022-01-04 22:02:19 +0800

    feat: add a vue demo

---
+++ vue/demo1/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="zs21-list-9">
+      <ul>
+        <li class="li-1">
+          <div class="list-con">
+            <div class="list-pic">
+              <a
+                href=""
+                target="_blank"
+                class="almg"
+                ><img :src="newsList[0].path" width="285" height="445" alt="" 
+              /></a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // newsList: []    // bad
+      newsList: [{}]  // good
+    }
+  },
+  created() {
+    axios.get('xxx').then(res => {
+      this.newsList = res;
+    })
+  }
+}
+</script>
+
