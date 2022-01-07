33d99a2c2fd0dc137d12d35d9336bac2b24f564e
wangxin <binaryaoe@163.com>
2022-01-07 09:46:17 +0800

    feat: circular queue test

--- algo/ms-top100/622.DesignCircularQueue/test.js
+++ algo/ms-top100/622.DesignCircularQueue/test.js
@@ -1,14 +1,22 @@
-const solution1 = require('./solution1.js')
+const solution1 = require('./solution3.js')
 const { calcOperateType } = require('../../utils/testRunner')
 
 const testcase = [
+  // {
+  //   input:
+  //   [
+  //     ["MyCircularQueue","enQueue","enQueue","enQueue","enQueue","Rear","isFull","deQueue","enQueue","Rear"],
+  //     [[3],[1],[2],[3],[4],[],[],[],[4],[]]
+  //   ],
+  //   expect: [null,true,true,true,false,3,true,true,true,4]
+  // },
   {
     input:
     [
-      ["MyCircularQueue","enQueue","enQueue","enQueue","enQueue","Rear","isFull","deQueue","enQueue","Rear"],
-      [[3],[1],[2],[3],[4],[],[],[],[4],[]]
+      ["MyCircularQueue","enQueue","enQueue","enQueue","enQueue","deQueue","deQueue","isEmpty","isEmpty","Rear","Rear","deQueue"],
+      [[8],[3],[9],[5],[0],[],[],[],[],[],[],[]]
     ],
-    expect: [null,true,true,true,false,3,true,true,true,4]
+    expect: [null,true,true,true,true,true,true,false,false,0,0,true]
   }
 ]
 
