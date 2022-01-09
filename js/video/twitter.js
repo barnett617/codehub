a82a92c48f59e465075b9edaf7198b847e8c0e1a
wangxin <binaryaoe@163.com>
2022-01-09 18:11:36 +0800

    feat: add a twitter detail api json demo

---
+++ js/video/twitter.js
@@ -0,0 +1,342 @@
+const TweetDetail = {
+  "data": {
+    "threaded_conversation_with_injections": {
+      "instructions": [{
+        "type": "TimelineAddEntries",
+        "entries": [{
+          "entryId": "tweet-1477296948462112772",
+          "sortIndex": "7746075088392663035",
+          "content": {
+            "entryType": "TimelineTimelineItem",
+            "itemContent": {
+              "itemType": "TimelineTweet",
+              "tweet_results": {
+                "result": {
+                  "__typename": "Tweet",
+                  "rest_id": "1477296948462112772",
+                  "core": {
+                    "user_results": {
+                      "result": {
+                        "__typename": "User",
+                        "id": "VXNlcjoxNDY3MTE4NTQwNDI4MDI5OTU1",
+                        "rest_id": "1467118540428029955",
+                        "affiliates_highlighted_label": {},
+                        "has_nft_avatar": false,
+                        "legacy": {
+                          "blocked_by": false,
+                          "blocking": false,
+                          "can_dm": false,
+                          "can_media_tag": true,
+                          "created_at": "Sat Dec 04 13:08:33 +0000 2021",
+                          "default_profile": true,
+                          "default_profile_image": false,
+                          "description": "Snowboarder / wakesuffer Instagram @88dsm",
+                          "entities": {
+                            "description": {
+                              "urls": []
+                            },
+                            "url": {
+                              "urls": [{
+                                "display_url": "youtu.be/aG57VnPGtuA",
+                                "expanded_url": "https://youtu.be/aG57VnPGtuA",
+                                "url": "https://t.co/ecMaysT7vd",
+                                "indices": [0, 23]
+                              }]
+                            }
+                          },
+                          "fast_followers_count": 0,
+                          "favourites_count": 21,
+                          "follow_request_sent": false,
+                          "followed_by": false,
+                          "followers_count": 3,
+                          "following": false,
+                          "friends_count": 73,
+                          "has_custom_timelines": false,
+                          "is_translator": false,
+                          "listed_count": 0,
+                          "location": "대한민국 서울",
+                          "media_count": 22,
+                          "muting": false,
+                          "name": "DoSangMin",
+                          "normal_followers_count": 3,
+                          "notifications": false,
+                          "pinned_tweet_ids_str": ["1468605778647203841"],
+                          "profile_image_extensions": {
+                            "mediaColor": {
+                              "r": {
+                                "ok": {
+                                  "palette": [{
+                                    "percentage": 74.34,
+                                    "rgb": {
+                                      "blue": 225,
+                                      "green": 199,
+                                      "red": 174
+                                    }
+                                  }, {
+                                    "percentage": 18.42,
+                                    "rgb": {
+                                      "blue": 27,
+                                      "green": 14,
+                                      "red": 6
+                                    }
+                                  }, {
+                                    "percentage": 3.7,
+                                    "rgb": {
+                                      "blue": 137,
+                                      "green": 98,
+                                      "red": 52
+                                    }
+                                  }, {
+                                    "percentage": 0.81,
+                                    "rgb": {
+                                      "blue": 22,
+                                      "green": 5,
+                                      "red": 109
+                                    }
+                                  }, {
+                                    "percentage": 0.5,
+                                    "rgb": {
+                                      "blue": 22,
+                                      "green": 6,
+                                      "red": 203
+                                    }
+                                  }]
+                                }
+                              }
+                            }
+                          },
+                          "profile_image_url_https": "https://pbs.twimg.com/profile_images/1469329301770174465/zdHyXUr7_normal.jpg",
+                          "profile_interstitial_type": "",
+                          "protected": false,
+                          "screen_name": "insta88dsm",
+                          "statuses_count": 24,
+                          "translator_type": "none",
+                          "url": "https://t.co/ecMaysT7vd",
+                          "verified": false,
+                          "want_retweets": false,
+                          "withheld_in_countries": []
+                        },
+                        "super_follow_eligible": false,
+                        "super_followed_by": false,
+                        "super_following": false
+                      }
+                    }
+                  },
+                  "legacy": {
+                    "created_at": "Sat Jan 01 15:13:34 +0000 2022",
+                    "conversation_id_str": "1477296948462112772",
+                    "display_text_range": [0, 39],
+                    "entities": {
+                      "media": [{
+                        "display_url": "pic.twitter.com/ncLoecy6BO",
+                        "expanded_url": "https://twitter.com/insta88dsm/status/1477296948462112772/video/1",
+                        "id_str": "1477296860633403398",
+                        "indices": [40, 63],
+                        "media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1477296860633403398/pu/img/RKK-s3XKWFD6e243.jpg",
+                        "type": "photo",
+                        "url": "https://t.co/ncLoecy6BO",
+                        "features": {},
+                        "sizes": {
+                          "large": {
+                            "h": 720,
+                            "w": 1280,
+                            "resize": "fit"
+                          },
+                          "medium": {
+                            "h": 675,
+                            "w": 1200,
+                            "resize": "fit"
+                          },
+                          "small": {
+                            "h": 383,
+                            "w": 680,
+                            "resize": "fit"
+                          },
+                          "thumb": {
+                            "h": 150,
+                            "w": 150,
+                            "resize": "crop"
+                          }
+                        },
+                        "original_info": {
+                          "height": 720,
+                          "width": 1280
+                        }
+                      }],
+                      "user_mentions": [],
+                      "urls": [],
+                      "hashtags": [{
+                        "indices": [0, 7],
+                        "text": "shorts"
+                      }, {
+                        "indices": [8, 21],
+                        "text": "snowboarding"
+                      }, {
+                        "indices": [22, 29],
+                        "text": "スノーボード"
+                      }, {
+                        "indices": [30, 39],
+                        "text": "сноуборд"
+                      }],
+                      "symbols": []
+                    },
+                    "extended_entities": {
+                      "media": [{
+                        "display_url": "pic.twitter.com/ncLoecy6BO",
+                        "expanded_url": "https://twitter.com/insta88dsm/status/1477296948462112772/video/1",
+                        "id_str": "1477296860633403398",
+                        "indices": [40, 63],
+                        "media_key": "7_1477296860633403398",
+                        "media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1477296860633403398/pu/img/RKK-s3XKWFD6e243.jpg",
+                        "type": "video",
+                        "url": "https://t.co/ncLoecy6BO",
+                        "additional_media_info": {
+                          "monetizable": false
+                        },
+                        "ext_media_color": {
+                          "palette": [{
+                            "percentage": 68.36,
+                            "rgb": {
+                              "blue": 73,
+                              "green": 74,
+                              "red": 78
+                            }
+                          }, {
+                            "percentage": 29.94,
+                            "rgb": {
+                              "blue": 131,
+                              "green": 131,
+                              "red": 131
+                            }
+                          }, {
+                            "percentage": 0.39,
+                            "rgb": {
+                              "blue": 41,
+                              "green": 44,
+                              "red": 89
+                            }
+                          }, {
+                            "percentage": 0.33,
+                            "rgb": {
+                              "blue": 55,
+                              "green": 81,
+                              "red": 98
+                            }
+                          }, {
+                            "percentage": 0.24,
+                            "rgb": {
+                              "blue": 72,
+                              "green": 89,
+                              "red": 92
+                            }
+                          }]
+                        },
+                        "mediaStats": {
+                          "viewCount": 11
+                        },
+                        "ext_media_availability": {
+                          "status": "Available"
+                        },
+                        "features": {},
+                        "sizes": {
+                          "large": {
+                            "h": 720,
+                            "w": 1280,
+                            "resize": "fit"
+                          },
+                          "medium": {
+                            "h": 675,
+                            "w": 1200,
+                            "resize": "fit"
+                          },
+                          "small": {
+                            "h": 383,
+                            "w": 680,
+                            "resize": "fit"
+                          },
+                          "thumb": {
+                            "h": 150,
+                            "w": 150,
+                            "resize": "crop"
+                          }
+                        },
+                        "original_info": {
+                          "height": 720,
+                          "width": 1280
+                        },
+                        "video_info": {
+                          "aspect_ratio": [16, 9],
+                          "duration_millis": 10376,
+                          "variants": [{
+                            "bitrate": 832000,
+                            "content_type": "video/mp4",
+                            "url": "https://video.twimg.com/ext_tw_video/1477296860633403398/pu/vid/640x360/7MitxVtm1sUfTTQd.mp4?tag=12"
+                          }, {
+                            "content_type": "application/x-mpegURL",
+                            "url": "https://video.twimg.com/ext_tw_video/1477296860633403398/pu/pl/fKm-pivDcPnyg1RM.m3u8?tag=12&container=fmp4"
+                          }, {
+                            "bitrate": 256000,
+                            "content_type": "video/mp4",
+                            "url": "https://video.twimg.com/ext_tw_video/1477296860633403398/pu/vid/480x270/oM-FIwSaVuEQL8Rp.mp4?tag=12"
+                          }, {
+                            "bitrate": 2176000,
+                            "content_type": "video/mp4",
+                            "url": "https://video.twimg.com/ext_tw_video/1477296860633403398/pu/vid/1280x720/toXZxkrkUIzIeGfw.mp4?tag=12"
+                          }]
+                        }
+                      }]
+                    },
+                    "favorite_count": 2,
+                    "favorited": false,
+                    "full_text": "#shorts #snowboarding #スノーボード #сноуборд https://t.co/ncLoecy6BO",
+                    "is_quote_status": false,
+                    "lang": "und",
+                    "possibly_sensitive": false,
+                    "possibly_sensitive_editable": true,
+                    "place": {
+                      "attributes": {},
+                      "bounding_box": {
+                        "coordinates": [
+                          [
+                            [128.243611126885, 37.2717791769662],
+                            [128.243611126885, 37.8201983010668],
+                            [128.752577836267, 37.8201983010668],
+                            [128.752577836267, 37.2717791769662]
+                          ]
+                        ],
+                        "type": "Polygon"
+                      },
+                      "contained_within": [],
+                      "country": "Republic of Korea",
+                      "country_code": "KR",
+                      "full_name": "Pyeongchang-gun, Republic of Korea",
+                      "name": "Pyeongchang-gun",
+                      "id": "018363221bae51d8",
+                      "place_type": "city",
+                      "url": "https://api.twitter.com/1.1/geo/id/018363221bae51d8.json"
+                    },
+                    "quote_count": 0,
+                    "reply_count": 0,
+                    "retweet_count": 0,
+                    "retweeted": false,
+                    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+                    "user_id_str": "1467118540428029955",
+                    "id_str": "1477296948462112772"
+                  },
+                  "quick_promote_eligibility": {
+                    "eligibility": "IneligibleNotProfessional"
+                  }
+                }
+              },
+              "tweetDisplayType": "Tweet",
+              "hasModeratedReplies": false
+            }
+          }
+        }]
+      }, {
+        "type": "TimelineTerminateTimeline",
+        "direction": "Top"
+      }]
+    }
+  }
+}
