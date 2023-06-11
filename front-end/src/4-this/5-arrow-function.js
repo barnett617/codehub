e27db19dfaa04fb534881728a180d6ba61375d2c
Wilson <binaryaoe@163.com>
2023-06-11 12:42:53 +0800

    feat: move front-end repo as a sub folder here

---
+++ front-end/src/4-this/5-arrow-function.js
@@ -0,0 +1,15 @@
+var globalObject = this;
+var foo = (() => this);
+console.log(foo() === globalObject); // true
+
+// 接着上面的代码
+// 作为对象的一个方法调用
+var obj = {foo: foo};
+console.log(obj.foo() === globalObject); // true
+
+// 尝试使用call来设定this
+console.log(foo.call(obj) === globalObject); // true
+
+// 尝试使用bind来设定this
+foo = foo.bind(obj);
+console.log(foo() === globalObject); // true
---
+++ front-end/src/README.md
@@ -0,0 +1,17 @@
+# front-end
+
+前端核心基础
+
+1. [数组](src/1-array/index.md)
+2. [继承](src/2-inheritance/index.md)
+3. [闭包](src/3-closure/index.md)
+4. [this](src/4-this/index.md)
+5. [promise](src/5-promise/index.md)
+6. [常用排序算法](src/6-sort-algorithm/index.md)
+7. [防抖节流](src/7-debouce-throttle/index.md)
+
+宏观范围
+
+1. 浏览器
+2. 网络通信
+3. 框架
